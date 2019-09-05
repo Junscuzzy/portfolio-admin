5daeaff3d66d4c8c73ec54e94f41e32515377d29
Julien <juliencaron18@gmail.com>
2019-09-05 23:10:50 +0200

    Remove base theme not used & add some utils

--- wp-content/themes/gatsby/functions.php
+++ wp-content/themes/gatsby/functions.php
@@ -7,155 +7,137 @@
  * @package Gatsby
  */
 
-if ( ! function_exists( 'gatsby_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function gatsby_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Gatsby, use a find and replace
-		 * to change 'gatsby' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'gatsby', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'gatsby' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'gatsby_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
-endif;
-add_action( 'after_setup_theme', 'gatsby_setup' );
-
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
+ * Sets up theme defaults and registers support for various WordPress features.
  *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
  */
-function gatsby_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'gatsby_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'gatsby_content_width', 0 );
+add_action( 'after_setup_theme', function () {
+
+    /*
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
+     */
+    add_theme_support( 'title-tag' );
+
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support( 'post-thumbnails' );
+
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'menu-1' => esc_html__( 'Primary', 'gatsby' ),
+    ) );
+
+} );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function gatsby_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'gatsby' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'gatsby' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'gatsby_widgets_init' );
+add_action( 'widgets_init', function () {
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'gatsby' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'gatsby' ),
+    ) );
+} );
 
 /**
  * Enqueue scripts and styles.
  */
-function gatsby_scripts() {
-	wp_enqueue_style( 'gatsby-style', get_stylesheet_uri() );
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style( 'gatsby-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'gatsby-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'gatsby-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'gatsby_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
+//    wp_enqueue_script(
+//        'gatsby-navigation',
+//        get_template_directory_uri() . '/js/navigation.js',
+//        array(),
+//        '20151215',
+//        true
+//    );
+} );
 
 /**
- * Custom template tags for this theme.
+ * Add Custom Post Types & Taxonomies
  */
-require get_template_directory() . '/inc/template-tags.php';
+add_action( 'init', function () {
+
+    // Post-type: Portfolio
+    register_post_type( 'portfolio', array(
+        'labels' => array(
+            'name' => __( 'Projets' ),
+            'singular_name' => __( 'Projet' )
+        ),
+        'menu_icon' => 'dashicons-portfolio',
+        'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'post-formats' ),
+        'public' => true,
+        'has_archive' => true,
+    ) );
+
+    // Taxonomy: Tags
+    register_taxonomy('project_tag',array('portfolio'), array(
+        'hierarchical' => false,
+        'labels' => array(
+            'name' => __( 'Tags' ),
+            'singular_name' => __( 'Tag' )
+        ),
+        'public' => true,
+        'show_admin_column' => true
+    ));
+
+    // Taxonomy: Categories
+    register_taxonomy('project_cat',array('portfolio'), array(
+        'hierarchical' => true,
+        'labels' => array(
+            'name' => __( 'Categories' ),
+            'singular_name' => __( 'Category' )
+        ),
+        'public' => true,
+        'show_admin_column' => true
+    ));
+
+} );
 
 /**
- * Functions which enhance the theme by hooking into WordPress.
+ * Hide Bluid-in "Post" Post-type
  */
-require get_template_directory() . '/inc/template-functions.php';
+
+// The Side Menu
+add_action( 'admin_menu', function () {
+    remove_menu_page( 'edit.php' );
+} );
+
+// The +New Post in Admin Bar
+add_action( 'admin_bar_menu', function ( $wp_admin_bar ) {
+    $wp_admin_bar->remove_node( 'new-post' );
+}, 999 );
+
+// The Quick Draft Dashboard Widget
+add_action( 'wp_dashboard_setup', function (){
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+}, 999 );
 
 /**
- * Customizer additions.
+ * Auto add "alt" attribute to image on upload from this title
+ *
+ * @link https://developer.wordpress.org/reference/hooks/add_attachment/
+ * @param int $post_ID ID de l'image
  */
-require get_template_directory() . '/inc/customizer.php';
+add_action( 'add_attachment', function ( $post_ID ) {
+    if ( wp_attachment_is_image( $post_ID ) ) {
+        $img_title = get_post( $post_ID )->post_title;
+        $img_title = preg_replace( '%\s*[-_\s]+\s*%', ' ', $img_title );
+        $img_title = ucwords( strtolower( $img_title ) );
 
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
+        update_post_meta( $post_ID, '_wp_attachment_image_alt', $img_title );
+    }
+} );
