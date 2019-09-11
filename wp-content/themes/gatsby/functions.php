fe4a3032fdd5b07971f1deac1e2e7b7dc08c3885
julien <juliencaron18@gmail.com>
2019-09-11 16:32:46 +0200

    remove frontend links

--- wp-content/themes/gatsby/functions.php
+++ wp-content/themes/gatsby/functions.php
@@ -7,38 +7,15 @@
  * @package Gatsby
  */
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
+// Sets up theme defaults and registers support for various WordPress features.
 add_action( 'after_setup_theme', function () {
-
-    /*
-     * Let WordPress manage the document title.
-     * By adding theme support, we declare that this theme does not use a
-     * hard-coded <title> tag in the document head, and expect WordPress to
-     * provide it for us.
-     */
     add_theme_support( 'title-tag' );
-
-    /*
-     * Enable support for Post Thumbnails on posts and pages.
-     *
-     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-     */
     add_theme_support( 'post-thumbnails' );
-
 } );
 
-/**
- * Add Custom Post Types & Taxonomies
- */
-add_action( 'init', function () {
 
-    // Post-type: Portfolio
+// Add Custom Post Types & Taxonomies
+add_action( 'init', function () {
     register_post_type( 'portfolio', array(
         'labels' => array(
             'name' => __( 'Projets' ),
@@ -51,7 +28,6 @@ add_action( 'init', function () {
         'has_archive' => true,
     ) );
 
-    // Taxonomy: Tags
     register_taxonomy('project_tag',array('portfolio'), array(
         'hierarchical' => false,
         'labels' => array(
@@ -63,7 +39,6 @@ add_action( 'init', function () {
         'show_admin_column' => true
     ));
 
-    // Taxonomy: Categories
     register_taxonomy('project_cat',array('portfolio'), array(
         'hierarchical' => true,
         'labels' => array(
@@ -74,29 +49,19 @@ add_action( 'init', function () {
         'show_in_rest' => true,
         'show_admin_column' => true
     ));
-
 } );
 
-/**
- * Hide Bluid-in "Post" Post-type
- */
 
-// The Side Menu
+// Hide Bluid-in "Post" Post-type
 add_action( 'admin_menu', function () {
     remove_menu_page( 'edit.php' );
 } );
-
-// The Quick Draft Dashboard Widget
 add_action( 'wp_dashboard_setup', function (){
     remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
 }, 999 );
 
-/**
- * Auto add "alt" attribute to image on upload from this title
- *
- * @link https://developer.wordpress.org/reference/hooks/add_attachment/
- * @param int $post_ID ID de l'image
- */
+
+// Auto add "alt" attribute to image on upload from this title
 add_action( 'add_attachment', function ( $post_ID ) {
     if ( wp_attachment_is_image( $post_ID ) ) {
         $img_title = get_post( $post_ID )->post_title;
@@ -107,9 +72,9 @@ add_action( 'add_attachment', function ( $post_ID ) {
     }
 } );
 
+
 // Add ACF options page
 if( function_exists('acf_add_options_page') ) {
-	
 	acf_add_options_page(array(
 		'page_title' 	=> __('Theme General Settings'),
 		'menu_title'	=> __('Theme Settings'),
@@ -117,8 +82,8 @@ if( function_exists('acf_add_options_page') ) {
 		'capability'	=> 'edit_posts',
 		'redirect'		=> true
 	));
-	
 }
+
 
 // Add real site name and remove default wordpress link
 add_action( 'wp_before_admin_bar_render', function () {
@@ -127,7 +92,8 @@ add_action( 'wp_before_admin_bar_render', function () {
     // 1. Remove unused menu items
     //$wp_admin_bar->remove_menu('wp-logo');		    // Remove the WordPress logo
     $wp_admin_bar->remove_menu('site-name');			// Remove the site name menu
-    //$wp_admin_bar->remove_menu('view-site');			// Remove the view site link
+    $wp_admin_bar->remove_menu('archive');
+    $wp_admin_bar->remove_menu('view');	
     $wp_admin_bar->remove_menu('updates');				// Remove the updates link
     $wp_admin_bar->remove_menu('comments');				// Remove the user details tab
     $wp_admin_bar->remove_menu('customize');			// Remove W3 total cache plugin link
@@ -142,5 +108,4 @@ add_action( 'wp_before_admin_bar_render', function () {
             'target' => '_blank'
         )
     ));
-
 }, 999 ); 
